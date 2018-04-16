cb442bad511a8fb8ae4978d7db2e59e972fbb916
Maxime Dufault <maxime_duf@hotmail.com>
2018-04-16 08:07:14 -0400

    ajout des fichiers manquant

---
+++ Stratego - version de base/Stratego/ClassesMetier/Coordonnee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego
+{
+    public class Coordonnee
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordonnee(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
