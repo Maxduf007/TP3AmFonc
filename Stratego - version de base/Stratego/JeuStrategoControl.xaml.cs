f65fda5a2eb3890102b5da458a45183342479fe3
Maxduf007 <maxime_duf@hotmail.com>
2018-05-01 20:31:04 -0400

    changement affichage des pions éliminés

--- Stratego - version de base/Stratego/JeuStrategoControl.xaml.cs
+++ Stratego - version de base/Stratego/JeuStrategoControl.xaml.cs
@@ -526,6 +526,8 @@ namespace Stratego
                         // Retrait de la pièce attaquée.
                         grdPartie.Children.Remove(GrillePieces[(int)caseCible.X][(int)caseCible.Y]);
                         GrillePieces[(int)caseCible.X][(int)caseCible.Y] = null;
+
+                        MettreAJourPionAdverseEliminees(reponse);
                     }
                     else if (reponse.PiecesEliminees.Count == 1 && reponse.PiecesEliminees[0] != attaquant
                             || reponse.PiecesEliminees.Count == 0)
@@ -539,6 +541,7 @@ namespace Stratego
                         {
                             attaquant.EstRevele = true;
                             affichageAttaquant = CreerAffichagePiece(attaquant);
+                            MettreAJourPionAdverseEliminees(reponse);
                         }
 
                         GrillePieces[(int)caseCible.X][(int)caseCible.Y] = affichageAttaquant;
@@ -588,25 +591,28 @@ namespace Stratego
 
         private void MettreAJourPionAdverseEliminees(ReponseDeplacement reponse)
         {
-            List<Piece> PieceElimineeAjustement;
-            StringBuilder strBldNomlbl = new StringBuilder("lbl");
+            List<Piece> LstPieceElimineeAjustement = new List<Piece>();
             /*// On crée un predicat pour vérifier dans LstPiecesEliminees si la pièce adverse qui vient d'être éliminée
             // a un type qui correspond à une autre pièce éliminée dans la liste.
             Predicate<Piece> PredicatePiece = (Piece p) => { return p == reponse.PiecesEliminees[0]; }; //lambda*/
-
-            PieceElimineeAjustement = LstPiecesEliminees.FindAll(p => p == reponse.PiecesEliminees[0]);
             
-            if(PieceElimineeAjustement.Count() > 0)
+            if(LstPiecesEliminees != null)
             {
-                PieceElimineeAjustement.Add(reponse.PiecesEliminees[0]);
+                LstPieceElimineeAjustement = LstPiecesEliminees.FindAll(p => p == reponse.PiecesEliminees[0]);
+                LstPieceElimineeAjustement.Add(reponse.PiecesEliminees[0]);
 
                 // On récupère le nom du type pour créer un nom de label
-                string strNomLbl = PieceElimineeAjustement.GetType().ToString();
+                string strNomLbl = LstPieceElimineeAjustement[0].GetType().ToString();
                 strNomLbl = strNomLbl.Substring(9);
-                strBldNomlbl.Append(strNomLbl);
+ 
+         
 
-                ((MainWindow)App.Current.MainWindow).
 
+            }
+            else
+            {
+                LstPiecesEliminees = new List<Piece>();
+                LstPiecesEliminees.Add(reponse.PiecesEliminees[0]);
             }
                 
            
@@ -652,5 +658,57 @@ namespace Stratego
                 System.Windows.Forms.Application.Restart();
             }
         }
-   }
+
+        /// <summary>
+        /// Finds a Child of a given item in the visual tree. 
+        /// </summary>
+        /// <param name="parent">A direct parent of the queried item.</param>
+        /// <typeparam name="T">The type of the queried item.</typeparam>
+        /// <param name="childName">x:Name or Name of child. </param>
+        /// <returns>The first parent item that matches the submitted type parameter. 
+        /// If not matching item can be found, a null parent is being returned.</returns>
+        public static T FindChild<T>(DependencyObject parent, string childName)
+           where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+    }
 }
